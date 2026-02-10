ecdd85053d15af3b0c223e3e6aa0229d648f4762
Gowda <2175596@cognizant.com>
2026-02-10 12:04:07 +0530

    Added new folder to repository

---
+++ backend/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "aws-bucket-demo"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "my_table" {
+  name           = "aws-dynamodb-demo"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
